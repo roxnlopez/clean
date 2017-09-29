2ec8f111c3bc3a9eab14587feb7f8a6601ea3d08
roxnlopez <roxnlopez@gmail.com>
2017-09-29 13:44:51 -0600

    added contact space took out php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 define('__ROOT__', dirname(dirname(__FILE__))); 
 require_once(__ROOT__.'/contact.php'); 
 ?> 
@@ -15,3 +15,4 @@ require_once(__ROOT__.'/contact.php');
 ?>
 
 
+ -->
