c7acb9591100055bc31858b15de8892df4a0fda2
roxnlopez <roxnlopez@gmail.com>
2017-09-29 12:56:40 -0600

    php ROOT

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,11 +1,16 @@
+<?php 
+define('__ROOT__', dirname(dirname(__FILE__))); 
+require_once(__ROOT__.'/contact.php'); 
+?> 
+
 <?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
+	$name = $_POST['c_name'];
+	$email = $_POST['c_email'];
+	$message = $_POST['c_message'];
 	$recipient = "roxnlopez@gmail.com";
 	$subject = "Subject from $name";
 	$mailheader = "From: $email \r\n";
-	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	mail($recipient, $subject, $mailheader) or die("Error!");
 	echo "Thank You! I will get back to you as soon as possible!" . " -" . "<a href='./contact.php'> Back to site</a>";
 ?>
 
